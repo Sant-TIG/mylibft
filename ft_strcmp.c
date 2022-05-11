6b41d20ad79889c0120001513b76972b649ffbbe
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-05-11 12:12:44 +0200

    Update ft_strcmp.c

--- ft_strcmp.c
+++ ft_strcmp.c
@@ -35,8 +35,8 @@ int	ft_strcmp(const char *str1, const char *str2)
 	{
 		if((unsigned char)*str1 != (unsigned char)*str2)
 			return (*str1 - *str2);
-    str1++;
-    str2++;
+		str1++;
+		str2++;
 	}
 	return (*str1 - *str2);
 }
