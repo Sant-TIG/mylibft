49f3d109c6daa6e0dfef0f8c845bb3cd1bf126f7
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-05-30 16:34:43 +0200

    Update ft_memcpy.c

--- strings functions/ft_memcpy.c
+++ strings functions/ft_memcpy.c
@@ -16,35 +16,32 @@ n   ---> the number of bytes to be copied
 
 DESCRIPTION
 ***********
-The ft_memcpy() function copies n bytes from the src memory area to the dst memory
-area. These memory areas must not overlap. In this case, use the ft_memmove()
+The ft_memcpy function copies n bytes from the src memory area to the dst memory
+area. These memory areas must not overlap. In these cases, use the ft_memmove()
 function instead.
 
-First, it verifies that the arrays passed as parameters exist. If so, it
-directly copies from left to right the content of the src block to the dst
-block. Is for this reason that memory spaces must not overlap.
-
-The ft_memmove() function copies the content from right to left avoiding
-overlapping between the memory spaces. Another way to allow overlapping is to copy
-the bytes into a temporary array and then copy these into the destination block.
+First, it verifies that the arrays passed as parameters exist. If so it copies,
+in order and directly from source, the first n bytes that follows the memory address
+stored in src. Is for this reason that memory spaces must not overlap, since the
+data could be overwritten.
 
 RETURN VALUE
 ************
 - A pointer to dst.
-- NULL.
+- If there is an error whith the arrays, NULL.
 */
 
 #include <stddef.h>
 #include <unistd.h>
+#include "strings.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	ssize_t	i;
 
 	i = -1;
-	//printf("dst = %s y src = %s\n", dst, src);
 	if (!dst || !src)
-		return (NULL);
+		return (FT_NULL);
 	while (++i < n)
 		*(char *)(dst + i) = *(char *)(src+ i);
 	return (dst);
@@ -57,9 +54,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	ssize_t	i;
 
 	i = -1;
-	printf("dst = %s y src = %s\n", dst, src);
 	if (!dst || !src)
-		return (NULL);
+		return (FT_NULL);
 	while (++i < n)
 		((char *)dst)[i] = ((char *)src)[i];
 	return (dst);
