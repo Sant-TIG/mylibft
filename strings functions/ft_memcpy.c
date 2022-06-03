29a6fb643c1a261f09799591f145a4e83aee464b
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-06-03 16:14:52 +0200

    Update ft_memcpy.c

--- strings functions/ft_memcpy.c
+++ strings functions/ft_memcpy.c
@@ -10,8 +10,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 PARAMETERS
 **********
-dst ---> the destination array
-src ---> the array to be copied
+dst ---> the pointer to the the destination array
+src ---> the pointer to the the array to be copied
 n   ---> the number of bytes to be copied
 
 DESCRIPTION
@@ -21,43 +21,29 @@ area. Before starting to copy, it verifies that the arrays passed as parameters
 exist. If so it copies, in order and directly from the source, the first n bytes
 that follows the memory address stored in src.
 
-Is for this reason that memory spaces must not overlap, since the data could be
+Is for this reason that memory areas must not overlap, since the data could be
 overwritten. In these cases, use the ft_memmove() function instead.
 
 RETURN VALUE
 ************
 - A pointer to dst.
 - If there is an error whith the arrays, NULL.
+
 */
 
-#include <stddef.h>
-#include <unistd.h>
-#include "strings.h"
+#include "../mylibft.h"
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	ssize_t	i;
+	size_t	i;
 
-	i = -1;
+	i = 0;
 	if (!dst || !src)
 		return (FT_NULL);
-	while (++i < n)
+	while (i < n)
+	{
 		*(char *)(dst + i) = *(char *)(src+ i);
+		i++;
+	}
 	return (dst);
 }
-
-/* ANOTHER WAY TO DO THE SAME
-
-void	*ft_memcpy(void *dst, const void *src, size_t n)
-{
-	ssize_t	i;
-
-	i = -1;
-	if (!dst || !src)
-		return (FT_NULL);
-	while (++i < n)
-		((char *)dst)[i] = ((char *)src)[i];
-	return (dst);
-}
-
-*/
