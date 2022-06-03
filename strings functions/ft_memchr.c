64b93be5271c3d0951d1f3f2865570fcd16ae123
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-06-03 15:50:35 +0200

    Update ft_memchr.c

--- strings functions/ft_memchr.c
+++ strings functions/ft_memchr.c
@@ -10,18 +10,18 @@ void	*ft_memchr(const void *str, int c, size_t n);
 
 PARAMETERS
 **********
-str ---> the pointer to the memory space where the search is performed
+str ---> the pointer to the memory area where the search is performed
 c   ---> the character to find
 n   ---> the number of bytes to be analyzed
 
 DESCRIPTION
 ***********
 The ft_memchr() function searches for the first ocurrence of the character 'c'
-in the first n bytes of the memory space pointed to by 'str'. Both 'c' and the
-bytes of the memory space are interpreted as unsigned characters.
+in the first n bytes of the memory area pointed to by 'str'. Both 'c' and the
+bytes of the memory area are interpreted as unsigned characters.
 
 If it finds, it returns a pointer to its first occurrence so that we would have
-acces to the rest of the memory space starting from that character.
+acces to the rest of the memory area starting from that character.
 
 RETURN VALUE
 ************
@@ -30,8 +30,7 @@ RETURN VALUE
 
 */
 
-#include <stddef.h>
-#include "strings.h"
+#include "../mylibft.h"
 
 void	*ft_memchr(const void *str, int c, size_t n)
 {
