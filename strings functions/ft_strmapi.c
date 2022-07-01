74961ab6b928aa791750cb036b5178e96eb08904
Santi Pérez <90222276+Sant-TIG@users.noreply.github.com>
2022-07-01 17:30:33 +0200

    Update ft_strmapi.c

--- strings functions/ft_strmapi.c
+++ strings functions/ft_strmapi.c
@@ -16,8 +16,9 @@ f   ---> the function to apply to each character
 DESCRIPTION
 ***********
 The ft_strmapi() function applies the function 'f' to each character of the
-string 'str' to create  new string resulting from successive applications of
-the function. This new string will be allocated in a dinamic memory space.
+string 'str' to create a new string resulting from successive applications of
+the function. This new string will be allocated in a dinamic memory space using
+the malloc() C function.
 
 It first checks if 'str' is not NULL and then reserves the memory space for the
 new string 'dst', which will be the same size as the original string.
